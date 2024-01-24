57e7032780bdb5cc3831bb58434af556ac174b78
Niklas Hietala <niklas.hietala1997@gmail.com>
2024-01-24 10:57:44 +0200

    Remove button oonly shows to user that made a post

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,18 +1,16 @@
-import { useState } from 'react'
-import blogService from '../services/blogs'
+import { useState } from "react";
+import blogService from "../services/blogs";
 
 const Blog = ({ blog, user }) => {
+  const [view, setView] = useState(false);
+  const [updatedBlog, setUpdatedBlog] = useState(blog);
 
-  const [view, setView] = useState(false)
-  const [updatedBlog, setUpdatedBlog] = useState(blog)
-
-
-  console.log(updatedBlog)
+  console.log(updatedBlog);
 
   //Change view
   const viewChange = () => {
-    setView(!view)
-  }
+    setView(!view);
+  };
 
   //Like blog
   const handleLike = async () => {
@@ -21,59 +19,58 @@ const Blog = ({ blog, user }) => {
         title: updatedBlog.title,
         author: updatedBlog.author,
         url: updatedBlog.url,
-        likes: updatedBlog.likes + 1
+        likes: updatedBlog.likes + 1,
       });
       setUpdatedBlog(updated);
-      console.log(updated)
+      console.log(updated);
     } catch (exception) {
-      console.log('error');
+      console.log("error");
     }
-  }
+  };
 
   //Remove blog
   const handleRemove = async () => {
-      if(user.username !== blog.user.username){
-      alert('This is not your blog')
-      }
-      else if((window.confirm(`Remove blog ${blog.title} by ${blog.author}`)))
-        {
-          try {
+    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}`)) {
+      try {
         await blogService.remove(updatedBlog.id);
         setUpdatedBlog(null);
       } catch (exception) {
-        console.log('error');
+        console.log("error");
       }
-  }
-}
+    }
+  };
 
   //Render blog view based on view state
   const blogView = () => {
-    const username = blog.user.username
+    const username = blog.user.username;
     return (
       <div>
-      {updatedBlog &&
-        <section className='insideBlog'>
-          <p>{updatedBlog.title} by {updatedBlog.author}</p>
-          {view && (
-            <>
-              <p>URL: {updatedBlog.url}</p>
-              <p>Likes: {updatedBlog.likes} <button onClick={handleLike}>Like</button></p>
-              <p>{username}</p>
-            </>
-          )}
-          <button onClick={viewChange}>{view ? "hide" : "view"}</button>
-          <button onClick={handleRemove}>Remove</button>
-        </section>
-          }
+        {updatedBlog && (
+          <section className="insideBlog">
+            <p>
+              {updatedBlog.title} by {updatedBlog.author}
+            </p>
+            {view && (
+              <>
+                <p>URL: {updatedBlog.url}</p>
+                <p>
+                  Likes: {updatedBlog.likes}{" "}
+                  <button onClick={handleLike}>Like</button>
+                </p>
+                <p>{username}</p>
+              </>
+            )}
+            <button onClick={viewChange}>{view ? "hide" : "view"}</button>
+            {user.username === blog.user.username && (
+              <button onClick={handleRemove}>Remove</button>
+            )}
+          </section>
+        )}
       </div>
     );
-  }
+  };
 
-  return (
-    <div>
-      {blogView()}
-    </div>  
-  )
-}
+  return <div>{blogView()}</div>;
+};
 
-export default Blog
+export default Blog;
